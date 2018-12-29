72ff93e947354fe47cc7f8b99aa6869649361ca4
ovuncuzun <ovuncuzun@gmail.com>
2018-12-29 15:55:22 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Northwind';
+  public options={
+    position:["bottom","right"],
+    timeOut:3000,
+    lastOnBottom:true
+  }
 }
